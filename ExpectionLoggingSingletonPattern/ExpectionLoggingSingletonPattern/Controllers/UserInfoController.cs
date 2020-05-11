0c2befaf0a6b4015e5c8903bda2f5b6dbd9294cb
Sai Akula <saikiranakula@hotmail.com>
2020-05-11 00:20:03 -0500

    expection logging using singleton pattern

---
+++ ExpectionLoggingSingletonPattern/ExpectionLoggingSingletonPattern/Controllers/UserInfoController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ExpectionLoggingSingletonPattern.Models;
+using Logger;
+
+namespace ExpectionLoggingSingletonPattern.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        private ILog _logger;
+        public UserInfoController()
+        {
+            _logger = LogException.GetInstance;
+        }
+        private UserDetailsEntities db = new UserDetailsEntities();
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            _logger.LogException(filterContext.Exception.ToString());
+            filterContext.ExceptionHandled = true;
+            this.View("Error").ExecuteResult(this.ControllerContext);
+        }
+
+        // GET: UserInfo
+        public ActionResult Index()
+        {
+            return View(db.UserInfoes.ToList());
+        }
+
+        // GET: UserInfo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserInfo/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,City,Gender,PhoneNumber")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserInfoes.Add(userInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userInfo);
+        }
+
+        // GET: UserInfo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: UserInfo/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,City,Gender,PhoneNumber")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: UserInfo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            db.UserInfoes.Remove(userInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ExpectionLoggingSingletonPattern/Logger/LogException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Logger
+{
+    public sealed class LogException : ILog
+    {
+        private static readonly Lazy<LogException> instance = new Lazy<LogException>(() => new LogException());
+        private LogException()
+        {
+        }
+        public static LogException GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        } 
+
+        void ILog.LogException(string message)
+        {
+            String fileName = $"Exception_{DateTime.Now.ToString("yyyy-MM-dd")}.log";
+            String logPath = string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, fileName);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("----------------------");
+            sb.AppendLine(DateTime.Now.ToString());
+            sb.AppendLine(message);
+            using(StreamWriter sw = new StreamWriter(logPath,true))
+            {
+                sw.Write(sb.ToString());
+                sw.Flush();
+            }
+        }
+    }
+
+}
